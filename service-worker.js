3bbf1f8702a8228f36a3d3fe5b7e9779bb7366ed
Mark Paul <markpaulcaneda19@gmail.com>
2021-03-01 20:10:06 +0800

    updates in sw

--- service-worker.js
+++ service-worker.js
@@ -4,8 +4,30 @@ const CACHE_NAME  = 'Codexus';
 /* Add relative URL of all the static content you want to store in
  * cache storage (this will help us use our app offline)*/
 let resourcesToCache = [
-    "./", "./assets/media/logo-brand.png", "./assets/media/logo-brand.svg",  "./assets/media/computer-stat.svg", "./assets/media/banner.svg", "./assets/media/about.svg", "./assets/js/scripts.js", "./assets/css/style.css", "./assets/css/responsive.css"];
+    "./", 
+    "./assets/media/logos/laravel-logo.png",
+    "./assets/media/logos/bootstrap-logo.png",
+    "./assets/media/logos/html-logo.png",
+    "./assets/media/logos/css-logo.png",
+    "./assets/media/logos/sass-logo.png",
+    "./assets/media/logos/javascript-logo.png",
+    "./assets/media/logos/node-logo.png",
+    "./assets/media/logos/git-logo.png",
+    "./assets/media/logos/vue-logo.png",
+    "./assets/media/logos/amazon-web-services-logo.png",
+    "./assets/media/logos/wordpress-logo.png",
+    "./assets/media/logos/ionic-logo.png",
+    "./assets/media/logo-brand.png", 
+    "./assets/media/logo-brand.svg",  
+    "./assets/media/computer-stat.svg", 
+    "./assets/media/banner.svg", 
+    "./assets/media/about.svg", 
+    "./assets/js/scripts.js", 
+    "./assets/css/style.css", 
+    "./assets/css/responsive.css"
+];
 
+// Install
 self.addEventListener("install", e=>{
     e.waitUntil(
         caches.open(CACHE_NAME).then(cache =>{
